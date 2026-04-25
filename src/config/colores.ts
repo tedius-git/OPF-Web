fe361f890fb282a8e4b99c03aae18175e60077b8
tedius-git <diegominasantos@gmail.com>
2026-04-24 22:56:47 -0600

    feat(gradientes): gradientes naranja y azul en colores.ts

--- src/config/colores.ts
+++ src/config/colores.ts
@@ -9,6 +9,8 @@ export const colores = {
   negro: "#1A1A1A",
   gris: "#DEDAD4",
   blanco: "#FFFFFF",
+  gradienteNaranja: "linear-gradient(to right, #B85025, #C8960C)",
+  gradienteAzul: "linear-gradient(to right, #1F6BED, #23498C)",
 } as const;
 
 export type Color = keyof typeof colores;
